82f7b63ec1858378e11672f210e11947c0b9217b
Ian Jones <enigma994@live.co.uk>
2014-10-22 00:45:31 +0100

    Adding of base project, proges includes gravity simulation working, space Skybox, Camera controls and rudimentry info tab when targeting bodies (though targeting bodies has been disabled while work is done on camera controls).

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	Vector3 DEFAULTTARGETVECTOR = new Vector3(0,0,10);
+
+	public GameObject target;
+
+	float x, y;
+	float zoom = 10;
+
+	Vector3 targetVector;
+
+	// Use this for initialization
+	void Start () 
+	{
+		targetVector  = DEFAULTTARGETVECTOR;
+		transform.position = Vector3.zero;
+	}
+
+	void OnGUI()
+	{
+		if(target != null)
+		{
+			GUI.Box(new Rect(0,0,160,90), "Target Info");
+			GUI.Label(new Rect(5,15,100,90), "Name: " + target.name);
+			GUI.Label(new Rect(5,30,100,90), "Size: " + target.transform.localScale.x.ToString("0.00"));
+			GUI.Label(new Rect(5,45,100,90), "Mass: " + target.rigidbody.mass.ToString("0.00"));
+			GUI.Label(new Rect(5,60,150,90), "Velocity" + target.rigidbody.velocity.ToString());
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButton(2))
+		{
+			x -= Input.GetAxis("Mouse X") ;
+			y -= Input.GetAxis("Mouse Y");
+
+			transform.position = new Vector3(x,y,0);
+		}
+		else
+		{
+
+			if(Input.GetKey(KeyCode.W))
+			{
+				rigidbody.AddForce(Vector3.forward * 2);
+			}
+
+			if(Input.GetKey(KeyCode.S))
+			{
+				rigidbody.AddForce(Vector3.back * 2);
+			}
+
+			if(Input.GetKey(KeyCode.A))
+			{
+				rigidbody.AddForce(Vector3.left * 2);
+			}
+
+			if(Input.GetKey(KeyCode.D))
+			{
+				rigidbody.AddForce(Vector3.right * 2);
+			}
+
+		}
+		if(Input.GetMouseButton(1))
+		{
+
+			x += Input.GetAxis("Mouse X");
+			y -= Input.GetAxis("Mouse Y");
+						
+			Quaternion rotation = Quaternion.Euler(y, x, 0);
+			
+			transform.rotation = rotation;
+		}
+
+		if(Input.GetKeyDown(KeyCode.LeftAlt))
+	   	{
+			targetVector = transform.position + Vector3.forward*-10;
+		}
+
+		if(Input.GetKey(KeyCode.LeftAlt))
+		{
+			if(target == null)
+			{
+				x += Input.GetAxis("Mouse X");
+				y -= Input.GetAxis("Mouse Y");
+
+				Quaternion rotation = Quaternion.Euler(y, x, 0);
+				Vector3 position = rotation * (new Vector3(0,0,10) + targetVector);
+
+//				var rotation = Quaternion.Euler(y, x, 0);
+//				var position = rotation * Vector3(0.0, 0.0, -distance) + target.position;
+
+				
+				transform.rotation = rotation;
+				transform.position = position;
+			}
+		}
+
+//		if(Input.GetMouseButtonDown(0))
+//		{
+//			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//			RaycastHit hit;
+//			
+//			if(Physics.Raycast(ray, out hit))
+//			{
+//				if(target != GameObject.Find(hit.transform.name))
+//				{
+//					target = GameObject.Find(hit.transform.name);
+//					zoom += target.transform.localScale.z;
+//				}
+//			}
+//			else
+//			{
+//				target = null;
+//			}
+//		}
+
+	}
+}
---
+++ Assets/Scripts/SpaceManager.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpaceManager : MonoBehaviour 
+{
+	//How many objects you want to start with
+	public int startingObjects = 5;
+	//Prefab used for spawning planets
+	public GameObject body;
+
+	List<GameObject> bodies;
+
+	int gForceAmp = 50;
+	// Use this for initialization
+	void Start () 
+	{
+		bodies = new List<GameObject>();
+
+		bodies.Add(Instantiate(body, new Vector3(0, 0.0f, 0.0f), Quaternion.identity) as GameObject);
+		bodies[bodies.Count-1].name = "body" + (bodies.Count-1);
+		//bodies.Add(Instantiate(body, new Vector3(-5, 0.0f, 0.0f), Quaternion.identity) as GameObject);
+		//bodies.Add(Instantiate(body, new Vector3(0, 5f, 0.0f), Quaternion.identity) as GameObject);
+
+//		for(int i = 0; i < startingObjects; i++)
+//		{
+//			CBodies.Add(Instantiate(CBody, new Vector3(i * 5, 0.0f, 0.0f), Quaternion.identity) as GameObject);
+//			Debug.Log("Object Added");
+//		}
+
+		shuffleMass();
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if(bodies == null)
+		{
+			Debug.LogError("List is null");
+		}
+
+		foreach(GameObject firstBody in bodies)
+		{
+			if(firstBody == null)
+			{
+				Debug.LogError("I is null");
+			}
+			foreach(GameObject secondBody in bodies)
+			{
+				if(secondBody == null)
+				{
+					Debug.LogError("J is null");
+				}
+
+				if(firstBody.transform.position != secondBody.transform.position)
+				{	
+					//Find vector from first body to second body
+					Vector3 deltaPosition = secondBody.transform.position - firstBody.transform.position;
+					Debug.Log ("deltaPosition: " + deltaPosition.ToString());
+
+					Vector3 direction = Vector3.Normalize(deltaPosition);
+					Debug.Log("direction: " + direction.ToString());
+
+					Debug.DrawRay(firstBody.transform.position, direction, Color.red);
+
+					float sqrRadius = deltaPosition.sqrMagnitude;
+					Debug.Log ("Square Radius: " + sqrRadius);
+
+					float forcedueToGrav = (firstBody.rigidbody.mass * secondBody.rigidbody.mass)/sqrRadius;
+					Debug.Log ("Force Due To Gravity: " + forcedueToGrav * gForceAmp);
+
+					firstBody.rigidbody.AddForce(direction * forcedueToGrav * gForceAmp);
+
+				}
+			}
+		}
+
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			bodies.Add(Instantiate(body, new Vector3(5, 5, 5), Quaternion.identity) as GameObject);
+			bodies[bodies.Count -1].rigidbody.AddForce(new Vector3(200, 0, 0));
+			bodies[bodies.Count -1].name = "body" + (bodies.Count-1);
+		}
+
+	}
+
+	void shuffleMass()
+	{
+		foreach(GameObject body in bodies)
+		{
+			float randomMass = Random.value + 0.1f;
+
+			if(randomMass > 10)
+			{
+				Debug.LogError("Mass greater than 10, unstability in simulation may occur");
+			}
+			else
+			{
+				body.rigidbody.mass = randomMass;
+			}
+
+			body.transform.localScale *= (randomMass * 50f);
+
+		}
+	}
+
+	public void removeBodyAt(Vector3 position)
+	{
+		foreach(GameObject body in bodies.ToArray())
+		{
+			if(body.transform.position == position)
+			{
+				bodies.Remove(body);
+				Destroy(body);
+			}
+
+		}
+	}
+}
+	
